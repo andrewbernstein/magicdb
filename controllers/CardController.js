0889a5ea61b3b22e277e7ef91962c7494dbb6e44
Andrew Bernstein <andrewlbernstein@gmail.com>
2014-04-06 17:04:17 -0700

    Fixing card search to lcase the input card name

--- controllers/CardController.js
+++ controllers/CardController.js
@@ -2,8 +2,9 @@ var MongoService = require('../services/MongoService');
 
 var getCard = function(cardName, callback) {
 	MongoService.connect(function(db) {
+		var lcaseName = cardName.toLowerCase();
 		var rawCardsCollection = db.collection('cards');
-		rawCardsCollection.find({ lcaseName: cardName }).toArray(function(err, results) {
+		rawCardsCollection.find({ lcaseName: lcaseName }).toArray(function(err, results) {
 			callback(results, callback);
 		})
 	});
