3de22253e26553ae480eceb00c1de1feb7dc246d
Andrew Bernstein <andrewlbernstein@gmail.com>
2014-04-08 12:50:18 -0700

    Changing how images are loaded in so they can be loaded from mtgdb's image source or mine, once I rehost all the images

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,41 +1,57 @@
 var SearchController = require('./../controllers/CardSearchController');
 var CardController = require('./../controllers/CardController');
 var KeywordsController = require('./../controllers/KeywordsController');
+var config = require('config');
 
 var indexGet = function(req, res) {
-	//TODO: Not all of these work!
-	var setAbbreviations = KeywordsController.getPrintingSets();
-	res.render('index', {sets: setAbbreviations});
+	var pageData = {};
+	pageData.imageBaseUrl = config.images.baseUrl;
+
+	pageData.sets = KeywordsController.getPrintingSets();
+	res.render('index', pageData);
 };
 exports.indexGet = indexGet;
 
 var searchGet = function(req, res) {
-	res.render('search');
+	var pageData = {};
+	pageData.imageBaseUrl = config.images.baseUrl;
+
+	res.render('search', pageData);
 };
 exports.searchGet = searchGet;
 
 var searchPost = function(req, res) {
-	console.log(req.body);
+	var pageData = {};
+	pageData.imageBaseUrl = config.images.baseUrl;
+
 	var searchQuery = req.body.query;
 	SearchController.cardSearch(searchQuery, function(results) {
-		res.render('search', { results: results });
+		pageData.results = results;
+		res.render('search', pageData);
 	});
 };
 exports.searchPost = searchPost;
 
 var cardGet = function(req, res) {
+	var pageData = {};
+	pageData.imageBaseUrl = config.images.baseUrl;
+
 	var cardName = decodeURIComponent(req.params.cardName);
-	console.log(cardName);
 	CardController.getCard(cardName, function(results) {
-		res.render('singleCard', { results: results });
+		pageData.results = results;
+		res.render('singleCard', pageData);
 	});
 };
 exports.cardGet = cardGet;
 
 var setGet = function(req, res) {
+	var pageData = {};
+	pageData.imageBaseUrl = config.images.baseUrl;
+	
 	var setName = decodeURIComponent(req.params.setName);
 	CardController.getSet(setName, function(results) {
-		res.render('search', { results: results });
+		pageData.results = results;
+		res.render('search', pageData);
 	});
 }
 exports.setGet = setGet;
