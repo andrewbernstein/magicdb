05748122d2815a53bf2b78d9b44e7684bf717bfb
Andrew Bernstein <andrewlbernstein@gmail.com>
2014-07-05 22:03:08 -0700

    Adding quick filters to search and set pages

--- static/js/magicdb-ng.js
+++ static/js/magicdb-ng.js
@@ -26,6 +26,16 @@ angular.module('project', [ 'ngRoute' ])
 				redirectTo:'/'
 			});
 	})
+	.filter('colorFilter', function() {
+		return function(cards, color) {
+			return cards.filter(function(card) {
+				if(color == '' || card.colors.indexOf(color) !== -1) {
+					return true;
+				}
+				return false;
+			});
+		}
+	})
 	.factory('Search', [
 		'$location',
 		function($location) {
@@ -37,6 +47,35 @@ angular.module('project', [ 'ngRoute' ])
 			}
 		}]
 	)
+	.factory('RarityComparator', function() {
+		return function(actual, expected) {
+			//we have to check inside the printings of the card to see if
+			if(!expected) {
+				return true;
+			}
+			var res = false;
+			actual.forEach(function(a) {
+				/*
+				 Here's what I don't get: expected is an object {rarity: "Rare"}
+				 For the color comparator above, it isn't.  If anyone can explain this, let me know.
+				 */
+				if(a.rarity == expected.rarity) {
+					res = true;
+				}
+			});
+			return res;
+		};
+	})
+	.factory('ColorComparator', function() {
+		return function(actual, expected) {
+			//colors is an array of the card's colors, so we can't use angular's default filter :/
+			//therefore, we use check to see if the card has the expected color inside the array
+			if(!expected) {
+				return true;
+			}
+			return actual.indexOf(expected) != -1;
+		};
+	})
 	.controller('MainController', [
 		'$scope', '$location', '$timeout', '$http', 'Search',
 		function($scope, $location, $timeout, $http, Search) {
@@ -51,10 +90,12 @@ angular.module('project', [ 'ngRoute' ])
 		}
 	])
 	.controller('SearchController', [
-		'$scope', '$location', '$routeParams', '$timeout', '$http', 'Search',
-		function($scope, $location, $routeParams, $timeout, $http, Search) {
+		'$scope', '$location', '$routeParams', '$timeout', '$http', 'Search', 'RarityComparator', 'ColorComparator',
+		function($scope, $location, $routeParams, $timeout, $http, Search, RarityComparator, ColorComparator) {
 			$scope.performSearch = Search;
 			$scope.searchparam = $routeParams.search;
+			$scope.colorComparator = ColorComparator;
+			$scope.rarityComparator = RarityComparator;
 			if($routeParams.search) {
 				$http.get('/json/search/' + $routeParams.search).success(function(results) {
 					$scope.cards = results;
@@ -74,9 +115,12 @@ angular.module('project', [ 'ngRoute' ])
 		}
 	])
 	.controller('SetController', [
-		'$scope', '$location', '$routeParams', '$timeout', '$http', 'Search',
-		function($scope, $location, $routeParams, $timeout, $http, Search) {
+		'$scope', '$location', '$routeParams', '$timeout', '$http', 'Search', 'RarityComparator', 'ColorComparator',
+		function($scope, $location, $routeParams, $timeout, $http, Search, RarityComparator, ColorComparator) {
 			$scope.performSearch = Search;
+			$scope.colorComparator = ColorComparator;
+			$scope.rarityComparator = RarityComparator;
+			$scope.colorText = '';
 			$http.get('/json/set/' + $routeParams.setName).success(function(results) {
 				$scope.cards = results;
 				$timeout(function() {
